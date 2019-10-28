70aa785b64b5f841b725519dc28e0aa0bc912b3e
katosan0904 <gachikiritanpo@gmail.com>
2019-10-28 16:04:40 +0900

    add s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    :provider              => 'AWS',
+    :region                => ENV['S3_REGION'], 
+    :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET_KEY']
+  }
+  config.fog_directory     =  ENV['S3_BUCKET']
+end
+
