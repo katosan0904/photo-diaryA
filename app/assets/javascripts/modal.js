f5d4d6ccb52c070015cf7008f89bec8124dd88fd
katosan0904 <gachikiritanpo@gmail.com>
2019-11-01 22:49:25 +0900

    add modal

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.trash-icon').on('click',function(){
+    $('.post-modal').fadeIn();
+    // 画面をスクロールさせない処理
+    $('body').css("overflow", "hidden");
+    return false;
+  });
+  $('.post-modal__window--cancel').on('click',function(){
+    $('.post-modal').fadeOut();
+    $('body').css("overflow", "scroll");
+  });
+  $('..post-modal__window--ok').on('click', function(){
+    $('.modal').fadeOut();
+  });
+});
