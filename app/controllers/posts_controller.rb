8e6e2f51f4eafa46aa9b912d0aa64c903f3e5f8d
katosan0904 <gachikiritanpo@gmail.com>
2019-11-01 17:05:04 +0900

    add paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.page(params[:page]).per(6).order("created_at DESC")
   end
   
   def show
