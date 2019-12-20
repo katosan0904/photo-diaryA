4bc9a4a845e3646495ffab2a5a5ce71f70adeb05
katosan0904 <gachikiritanpo@gmail.com>
2019-12-20 15:50:24 +0900

    Update 20191118070258_change_column_to_post.rb

--- db/migrate/20191118070258_change_column_to_post.rb
+++ db/migrate/20191118070258_change_column_to_post.rb
@@ -1,6 +1,6 @@
 class ChangeColumnToPost < ActiveRecord::Migration[5.0]
   def change
-    change_column_null :Posts, :image, null: false
-    change_column_null :Posts, :category_id, null: false
+    change_column_null :posts, :image, null: false
+    change_column_null :posts, :category_id, null: false
   end
 end
