9743191af1504cc2438d37fa922e4c9d8c385751
katosan0904 <gachikiritanpo@gmail.com>
2019-12-20 17:48:55 +0900

    add s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,17 @@
-# if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.storage = :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       :provider              => 'AWS',
-#       :region                => ENV['S3_REGION'], 
-#       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-#       :aws_secret_access_key => ENV['S3_SECRET_KEY']
-#     }
-#     config.fog_directory     =  ENV['S3_BUCKET']
-#   end
-# end
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'photo-diarya'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/photo-diarya'
+end
